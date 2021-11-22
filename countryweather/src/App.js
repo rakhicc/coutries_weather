20a243588ac7cd6db438f73dcd32799deaabf2d1
BC-macair032 <bc-macair032@BC-macair032s-MacBook-Air.local>
2021-11-22 15:54:13 +0200

    added fetch and styling to country details

--- countryweather/src/App.js
+++ countryweather/src/App.js
@@ -1,11 +1,42 @@
 import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
+import { data } from "browserslist";
+class App extends Component {
+  state = {
+    data: [],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hekko</h1>
-    </div>
-  );
+  componentDidMount() {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      this.setState({ data: res.data });
+      //console.log(this.state.data);
+    });
+  }
+
+  render() {
+    /* const countryname = this.state.data.map((c) => {
+      return (
+        <div key={c.name.common}>
+          <h2>{c.name.common}</h2>
+          <p>{c.capital}</p>
+          <img src={c.flags.png} />
+        </div>
+      );
+    }); */
+    return (
+      <div className="countries">
+        {this.state.data.map((c) => (
+          <div key={c.name.common}>
+            <h2>{c.name.common}</h2>
+            <p>{c.capital}</p>
+            <img src={c.flags.png} />
+          </div>
+        ))}
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
